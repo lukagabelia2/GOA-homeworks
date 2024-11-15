0d929c1f1092e36c334aa2f160484f7f3fab87b3
lukagabelia2 <lukagabelia26@gmail.com>
2024-11-15 22:43:59 +0400

    fourth commit

---
+++ day04/classwork/cw.py
@@ -0,0 +1,29 @@
+# name = "luka"
+# numb = 10
+# numb2 = 10.0
+
+# name = "luka"
+# surname = "gabelia"
+
+# print(name + surname)
+
+# num = "10"
+# num2 = "20"
+
+# print("10" + "20")
+# print("GOA BEST")
+# print(10)
+
+
+name = "luka"#str
+num = 1 #itr
+num2 = 25.3 #float
+
+
+num3 = 20
+num4 = 50
+
+print(num3 + num4)
+print(num3 - num4)
+print (num3 / num4)
+print(num3 * num4)
