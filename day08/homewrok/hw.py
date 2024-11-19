21410728fc8bbf3ac4de9361e411a7960b0db6fc
lukagabelia2 <lukagabelia26@gmail.com>
2024-11-19 21:20:34 +0400

    eight commit

---
+++ day08/homewrok/hw.py
@@ -0,0 +1,5 @@
+a = 5
+b = "10"
+result = str(a) + b
+print("Result:", result)
+#აქ შეცომა იყო ის რომ int ემატებოდა str 
---
+++ day08/homewrok/hw2(პირობა).py
@@ -0,0 +1,4 @@
+value = "10"
+result = value * 5
+print("Result:", result)
+#გაიხსენეთ მონაცემთა ტიპები და შეასწორეთ ეს კოდი
